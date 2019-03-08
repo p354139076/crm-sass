a3c3f6d77d3055e5f6a3dfc4f8fa3ae62e53d1cb
panda <p354139076@qq.com>
2019-03-08 18:00:56 +0800

    config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import i18n from './lang' // Internationalization
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './errorLog' // error log
 import './icons' // icon
-import './permission'
+import './permission' // permission control
 import store from './store'
 Vue.use(ElementUI, {
 	size: 'medium', // set element-ui default size
