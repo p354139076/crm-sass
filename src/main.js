82f692d6dc3e592545d295aff7826c3598c97023
panda <p354139076@qq.com>
2019-03-11 10:33:48 +0800

    新增css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import i18n from './lang' // Internationalization
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // global css
 import './errorLog' // error log
 import './icons' // icon
 import './permission' // permission control
